b87959c3724c45a554add734c39ceb94a6a38776
Joe <48761685+joecausos@users.noreply.github.com>
2026-08-02 09:10:14 +0800

    Modified contents for timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -25,11 +25,11 @@ function Timeline() {
             <h4 className="vertical-timeline-element-subtitle">Cebu, Visayas, PH</h4>
             <br></br>
             <ul style={{ paddingLeft: '20px', margin: '0 0 15px 0' }}>
-              <li style={{ marginBottom: '5px' }}>Spearheaded defect-reduction initiatives, cutting PCB assembly errors by 37.78% through targeted process variable optimization.</li>
-              <li style={{ marginBottom: '5px' }}>Modeled equipment failure rates using Weibull and exponential distributions to predict performance degradation and maximize operational stability.</li>
-              <li style={{ marginBottom: '5px' }}>Executed systemic root-cause investigations via FMEA and Fault Tree Analysis to isolate bottlenecks and reduce Mean Time to Repair (MTTR).</li>
-              <li style={{ marginBottom: '5px' }}>Optimized maintenance schedules and equipment workflows by utilizing predictive modeling to identify and resolve performance degradation before failures occurred</li>
-              <li style={{ marginBottom: '5px' }}>Managed real-time MTBF and OEE reliability dashboards to streamline maintenance workflows and ensure strict ISO/IATF/IEC compliance.</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify'}}>Spearheaded defect-reduction initiatives, cutting PCB assembly errors by 37.78% through targeted process variable optimization.</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Modeled equipment failure rates using Weibull and exponential distributions to predict performance degradation and maximize operational stability.</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Executed systemic root-cause investigations via FMEA and Fault Tree Analysis to isolate bottlenecks and reduce Mean Time to Repair (MTTR).</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Optimized maintenance schedules and equipment workflows by utilizing predictive modeling to identify and resolve performance degradation before failures occurred</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Managed real-time MTBF and OEE reliability dashboards to streamline maintenance workflows and ensure strict ISO/IATF/IEC compliance.</li>
             </ul>
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -43,10 +43,10 @@ function Timeline() {
             <h4 className="vertical-timeline-element-subtitle">Olongapo, Luzon, PH</h4>
             <br></br>  
             <ul style={{ paddingLeft: '20px', margin: '0 0 15px 0' }}>
-              <li style={{ marginBottom: '5px' }}>Automated reporting and dashboarding workflows, slashing processing time by 60% and boosting tracking accuracy.</li>
-              <li style={{ marginBottom: '5px' }}>Resolved critical defects and cut identification-to-resolution time by 25% utilizing 8D, 5Why, and CAPA methodologies.</li>
-              <li style={{ marginBottom: '5px' }}>Streamlined quality control workflows through process mapping and targeted Kaizen initiatives to eliminate operational friction.</li>
-              <li style={{ marginBottom: '5px' }}>Secured multiple Quarterly Business Recognitions (2022, 2023, 2025) for driving sustained operational excellence and global ISO compliance.</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Automated reporting and dashboarding workflows, slashing processing time by 60% and boosting tracking accuracy.</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Resolved critical defects and cut identification-to-resolution time by 25% utilizing 8D, 5Why, and CAPA methodologies.</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Streamlined quality control workflows through process mapping and targeted Kaizen initiatives to eliminate operational friction.</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Secured multiple Quarterly Business Recognitions (2022, 2023, 2025) for driving sustained operational excellence and global ISO compliance.</li>
             </ul>
           </VerticalTimelineElement>
           
@@ -61,9 +61,9 @@ function Timeline() {
             <h4 className="vertical-timeline-element-subtitle">Olongapo, Luzon, PH</h4>
             <br></br>
             <ul style={{ paddingLeft: '20px', margin: '5px 0 0 0' }}>
-              <li style={{ marginBottom: '5px' }}>Conducted rigorous technical inspections to isolate and address performance failure points, maintaining strict quality control and operational stability</li>
-              <li style={{ marginBottom: '5px' }}>Analysed performance reports to replicate issues and implement effective corrective measures, ensuring long-term system reliability</li>
-              <li style={{ marginBottom: '5px' }}>Applied precise inspection methodologies to evaluate system viability and ensure all outputs met specified technical performance requirements</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Conducted rigorous technical inspections to isolate and address performance failure points, maintaining strict quality control and operational stability</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Analysed performance reports to replicate issues and implement effective corrective measures, ensuring long-term system reliability</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Applied precise inspection methodologies to evaluate system viability and ensure all outputs met specified technical performance requirements</li>
             </ul>
           </VerticalTimelineElement>
           
@@ -78,10 +78,10 @@ function Timeline() {
             <h4 className="vertical-timeline-element-subtitle">Olongapo, Luzon, PH</h4>
             <br></br>
             <ul style={{ paddingLeft: '20px', margin: '5px 0 0 0' }}>
-              <li style={{ marginBottom: '5px' }}>Performed comprehensive audits to ensure operational standards, identifying and documenting opportunities for system-wide performance enhancement</li>
-              <li style={{ marginBottom: '5px' }}>Developed and executed programs to improve efficiency and maintain high-quality output standards</li>
-              <li style={{ marginBottom: '5px' }}>Investigated quality complaints by conducting systematic root cause analyses and implementing preventive solutions to mitigate future risks</li>
-              <li style={{ marginBottom: '5px' }}>Collaborated on new product development to ensure strict adherence to quality and compliance standards</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Performed comprehensive audits to ensure operational standards, identifying and documenting opportunities for system-wide performance enhancement</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Developed and executed programs to improve efficiency and maintain high-quality output standards</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Investigated quality complaints by conducting systematic root cause analyses and implementing preventive solutions to mitigate future risks</li>
+              <li style={{ marginBottom: '5px', textAlign: 'justify' }}>Collaborated on new product development to ensure strict adherence to quality and compliance standards</li>
             </ul>
           </VerticalTimelineElement>
 
